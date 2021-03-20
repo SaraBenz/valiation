39631f465b2048cb06274c7ee93933042f9b2130
SaraBenz <saraben943@gmail.com>
2021-03-20 09:59:11 +0100

    rajout des controles

--- functions/util/validators.js
+++ functions/util/validators.js
@@ -4,9 +4,19 @@ const isEmail = (email) => {
     const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (email.match(regEx)) return true;
     else return false;
-
-
 }
+//nom
+const isNom = (nom) => {
+    const regEx = /^[A-Z][A-Za-z\é\è\ê\-]+$/;
+    if (nom.match(regEx)) return true;
+    else return false;
+}
+//pwd
+const isMdp = (mdp) => {
+    const regEx = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([a-zA-Z0-9]{8})$/;
+    if (mdp.match(regEx)) return true;
+    else return false;}
+    
 
 const isEmpty = (string) => {
     if (string.trim() === '') return true;
@@ -20,9 +30,20 @@ exports.validateSignupData =(data)=>{
     else if (!isEmail(data.email)) {
         errors.email = 'Must be a valid email adress';
     }
-    if (isEmpty(data.password)) errors.password = 'Must not be empty';
+//nom
+    if (isEmpty(data.nom)) {errors.nom = 'Must not be empty'} 
+    else if (!isNom(data.nom)) {
+        errors.nom = 'Must be a valid name';
+    }
+//mdp
+    if (isEmpty(data.password)) {errors.password = 'Must not be empty'}
+    else if (!isMdp(data.mdp)) {
+        errors.mdp = 'Must be a valid password';
+    }
+
+
     if (data.password !== data.confirmPassword) errors.confirmPassword = "passwords must match";
-    if (isEmpty(data.nom)) errors.nom = 'Must not be empty';
+    
     if (isEmpty(data.prenom)) errors.prenom = 'Must not be empty';
     if (isEmpty(data.prenom)) errors.prenom = 'Must not be empty';
 
